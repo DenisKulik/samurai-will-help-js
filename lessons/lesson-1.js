1313d96298779e891763c201af9a01b0085d34dc
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-09 14:27:24 +0700

    initial commit

---
+++ lessons/lesson-1.js
@@ -0,0 +1,105 @@
+'use strict';
+
+// Переменные
+
+// Типы данных
+
+// Преобразование типов
+
+// Операторы
+
+// переписать на тернарный оператор
+// const age = 18;
+// let status;
+// if (age >= 18) {
+//     status = 'adult';
+// } else {
+//     status = 'minor';
+// }
+
+// Инкремент и декремент
+
+// let counter = 0;
+// console.log(counter++);
+// console.log(++counter);
+// console.log(counter--);
+// console.log(++counter);
+// console.log(counter);
+
+// Операторы сравнения
+
+// console.log(1 == "1");
+// console.log(true == 1);
+// console.log(null == undefined);
+
+// console.log(1 === "1");
+// console.log(true === 1);
+// console.log(null === undefined);
+
+// console.log(1 <= '1');
+// console.log('2' >= '12');
+// console.log(false >= 1);
+
+// console.log('X' > 'x');
+// console.log('Banana' < 'Banan');
+
+// Логические операторы
+
+// || (ИЛИ)
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true || false);
+// console.log(false || false);
+
+// console.log(1 || 0);
+// console.log('' || 'Hello');
+// console.log(null || 1 || 'Hello' || '');
+// console.log(undefined || 0 || '' || null);
+
+// && (И)
+// console.log(true && true);
+// console.log(false && true && true);
+// console.log(true && false && false);
+// console.log(false && false);
+
+// console.log(null && 1);
+// console.log(1 && 2 && null && 3);
+// console.log(1 && 2 && 3);
+// console.log('Hello' && 1 && '');
+
+// ! (НЕ)
+// приводит аргумент к логическому типу true/false и возвращает противоположное значение
+// console.log(!true); // false
+// console.log(!0); // false
+
+// console.log(!!null);
+// console.log(!!'error message');
+
+// ?? (ОПЕРАТОР НУЛЕВОГО СЛИЯНИЯ)
+
+// let amount;
+// console.log(amount ?? 0);
+
+// let x = null;
+// let y = 'Hello';
+// let z;
+// console.log(x ?? y ?? z ?? 'default value');
+
+// циклы while/for
+
+// Напишите цикл while, который выводит в консоль числа от 1 до 5.
+// Напишите цикл for, который выводит в консоль квадраты чисел от 1 до 10.
+
+// Напишите цикл for, который выводит в консоль все элементы массива colors,
+// начиная с последнего элемента и заканчивая первым.
+// const colors = [ 'red', 'orange', 'yellow', 'green', 'blue', 'violet' ];
+
+// Напишите цикл for, который выводит в консоль все элементы двумерного массива matrix.
+// const matrix = [
+//     [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]
+// ];
+
+
+
+
+
