cfbf1cde21e2bab18330c38581a9adfab0bbf10d
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-23 15:53:47 +0700

    add lesson 3 start file

---
+++ lessons/lesson-3.js
@@ -0,0 +1,75 @@
+'use strict';
+
+// Arrays
+
+// Возвращаемый тип
+// console.log(typeof [1, 2, 3]);
+
+// Доступ к элементам массива
+// const carBrands = [ 'Mercedes', 'BMW', 'Volkswagen', 'Lexus', 'Audi', 'Opel' ];
+// вывести в консоль Audi
+
+// Перезапись элементов
+// Заменить 'BMW' на 'Tesla'
+
+// Получение длины массива
+// Как получить значение последнего элемента в массиве?
+
+// Destructuring assignment
+
+// Записать в переменную car3 Volkswagen
+
+// rest
+// Записать в переменную otherCars все остальные марки авто
+
+// Default values
+
+// Копирование массива
+// const carBrandsCopy = carBrands;
+// carBrandsCopy[0] = 'Tesla';
+// console.log(carBrands[0]);
+
+// Добавление/удаление элементов
+// const numbers = [ 1, 2, 3, 4, 5 ];
+// Добавить элемент 6 в конец массива
+
+// Удалить последний элемент из массива
+
+// const fruits = [ 'apple', 'banana', 'cherry' ];
+// Удалить первый элемент из массива
+
+// Добавить 'apple' элемент в начало массива
+
+// Какие методы затратнее по памяти?
+
+// Итерация по массивам
+// const nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+// for
+// Найти сумму всех элементов массива nums
+
+// Найти максимальное значение элемента в массиве nums
+
+// for of
+// Объединить все строки в массиве строк в одну строку.
+// const strings = [ 'Hello', 'world', '!' ];
+// console.log(combinedString); // 'Hello world !'
+
+// Написать функцию map, которая работает аналогично методу map
+
+// Дан массив со строками. Используя функцию map верните массив, сделав первую букву в словах заглавной.
+// const arr = [ 'apple', 'banana', 'car', 'dog', 'elephant' ];
+// console.log(reverseArr); // 'Apple', 'Banana', 'Car', 'Dog', 'Elephant'
+
+// Напишите функцию, которая принимает массив объектов и возвращает новый массив, содержащий только имена.
+// const people = [
+//     { name: 'John', age: 25 },
+//     { name: 'Jane', age: 30 },
+//     { name: 'Bob', age: 35 },
+//     { name: 'Michael', age: 21 },
+//     { name: 'Alice', age: 28 }
+// ];
+
+// console.log(names); // John, Jane, Bob, Michael, Alice
+
+
