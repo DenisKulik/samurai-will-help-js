ddcc38f52da25a448d0b7f62e5a5b86d191500b4
Denis Kulik <kulikdenis1996x@gmail.com>
2023-09-10 14:54:04 +0700

    lesson 10 start

---
+++ lessons/lesson-10.js
@@ -0,0 +1,301 @@
+// ============================ try/catch ======================================
+
+// ============================ async/await ====================================
+
+// переписать на async/await
+let isLoading = false
+const getTodosPr = () => {
+    isLoading = true
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => response.json())
+        .then((result) => {
+            console.log('result', result)
+        })
+        .catch((error) => {
+            console.log('error', error)
+        })
+        .finally(() => {
+            isLoading = false
+        })
+}
+
+//============================== call stack ================================
+
+const multiply = (x, y) => x * y
+
+const res1 = multiply(2, 3)
+const res2 = multiply(4, 5)
+const res3 = multiply(6, 7)
+
+const greet = (name) => {
+    console.log(`Hello, ${name}!`)
+}
+
+const sayHello = () => {
+    const myName = 'Alice'
+    greet(myName)
+    console.log('How are you?')
+}
+
+// Что выведется в консоль?
+// sayHello()
+
+// ============================ event loop =====================================
+const logMessage = (value) => {
+    console.log(value)
+}
+
+// logMessage('Start!')
+
+// Promise.resolve('I\'m Promise!').then((res) => logMessage(res))
+
+// logMessage('End!')
+
+// ============================= task queue ====================================
+
+// Что выведется в консоль?
+// =============================================================================
+// console.log(1)
+
+// const promise1 = new Promise((resolve, reject) => {
+//     console.log(2)
+// })
+
+// console.log(3)
+
+// =============================================================================
+// const log = (value) => {
+//     console.log(value)
+// }
+
+// const one = () => Promise.resolve(1)
+
+// const myFunc = async () => {
+//     log(2)
+//     const res = await one()
+//     log(res)
+// }
+
+// log(3)
+// myFunc()
+// log(4)
+
+// =============================================================================
+// console.log(1)
+
+// const promise2 = new Promise((resolve, reject) => {
+//     console.log(2)
+//     resolve(3)
+// })
+
+// promise2.then(res => {
+//     console.log(res)
+// })
+
+// console.log(4)
+
+// =============================================================================
+// console.log(1)
+
+// const promise1 = new Promise((resolve, reject) => {
+//     console.log(2)
+// })
+
+// promise1.then(res => {
+//     console.log(3)
+// })
+
+// console.log(4)
+
+// =============================================================================
+// console.log(1)
+
+// const promise3 = new Promise((resolve, reject) => {
+//     console.log(2)
+//     resolve(3)
+//     console.log(4)
+// })
+
+// promise3.then(res => {
+//     console.log(res)
+// })
+
+// console.log(5)
+
+// =============================================================================
+// console.log(1)
+
+// Promise.resolve(2).then((res) => {
+//     console.log(res)
+// })
+
+// Promise.resolve(3).then((res) => {
+//     console.log(res)
+// })
+
+// console.log(4)
+
+// =============================================================================
+// console.log(1)
+
+// const fn = () => (new Promise((resolve, reject) => {
+//     console.log(2)
+//     resolve(3)
+// }))
+
+// console.log(4)
+
+// fn().then(res => {
+//     console.log(res)
+// })
+
+// console.log(5)
+
+// =============================================================================
+
+// console.log(1)
+
+// setTimeout(() => {
+//     console.log(2)
+// })
+
+// Promise.resolve()
+//     .then(() => {
+//     console.log(3)
+// })
+
+// console.log(4)
+
+// =============================================================================
+
+// const promise4 = new Promise((resolve, reject) => {
+//     console.log(1)
+//     setTimeout(() => {
+//         console.log(2)
+//         resolve(3)
+//         console.log(4)
+//     }, 0)
+//     console.log(5)
+// })
+
+// promise4.then((res) => {
+//     console.log(res)
+// })
+
+// console.log(6)
+
+// =============================================================================
+// const timer1 = setTimeout(() => {
+//     console.log(1);
+
+//     const promise5 = Promise.resolve().then(() => {
+//         console.log(2)
+//     })
+// }, 0)
+
+// const timer2 = setTimeout(() => {
+//     console.log(3)
+// }, 0)
+
+// =============================================================================
+// console.log(1)
+
+// const promise6 = Promise.resolve().then(() => {
+//     console.log(2)
+//
+//     const timer2 = setTimeout(() => {
+//         console.log(3)
+//     }, 0)
+// })
+
+// const timer1 = setTimeout(() => {
+//     console.log(4)
+//
+//     const promise7 = Promise.resolve().then(() => {
+//         console.log(5)
+//     })
+// }, 0)
+
+// console.log(6)
+
+// =============================================================================
+// console.log(1)
+
+// setTimeout(() => console.log(2))
+
+// Promise.resolve().then(() => console.log(3))
+
+// Promise.resolve().then(() => setTimeout(() => console.log(4)))
+
+// Promise.resolve().then(() => console.log(5))
+
+// setTimeout(() => console.log(6))
+
+// console.log(7)
+
+// =============================================================================
+// console.log(1)
+
+// setTimeout(() => console.log(2))
+
+// Promise.reject(3).catch(console.log)
+
+// new Promise(resolve => setTimeout(resolve)).then(() => console.log(4))
+
+// Promise.resolve(5).then(console.log)
+
+// console.log(6)
+
+// setTimeout(() => console.log(7), 0)
+
+// =============================================================================
+// console.log(1)
+// setTimeout(() => {
+//     console.log(2)
+// }, 0)
+//
+// Promise
+//     .resolve()
+//     .then(() => {
+//         console.log(3)
+//     })
+//     .then(() => {
+//         console.log(4)
+//     })
+//
+// console.log(5)
+
+// =============================================================================
+// console.log(1)
+
+// setTimeout(() => console.log(2), 1000)
+
+// console.log(3)
+
+// new Promise((res) => res(4)).then((data) => console.log(data))
+
+// setTimeout(() => console.log(5), 2000)
+
+// Promise.resolve(6).then((data) => console.log(data))
+
+// new Promise((res) => setTimeout(() => res(7), 3000)).then((data) =>
+//     console.log(data)
+// )
+
+// =============================================================================
+// new Promise((res) => res(1)).then((data) => console.log(data))
+
+// setTimeout(() => console.log(2), 1000)
+
+// Promise.resolve(3).then((data) => console.log(data))
+
+// console.log(4)
+
+// new Promise((res) => setTimeout(() => res(7), 2000)).then((data) =>
+//     console.log(data)
+// )
+
+// for (let i = 0; i < 2; i++) {
+//     console.log(i)
+// }
+
